d74ad0638334f62530b8413bb53dd247fe729b47
jeanmariemcc <84250957+jeanmariemcc@users.noreply.github.com>
2021-11-15 14:57:05 -0500

    Add files via upload

--- models/Users.js
+++ models/Users.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Wiki = require("./Wiki");
 
 const usersSchema = new mongoose.Schema({
-	username: { type: String, required: true },
+	username: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
 	articles: [
 		{
--- models/Wiki.js
+++ models/Wiki.js
@@ -4,7 +4,11 @@ const Users = require("./Users");
 const wikiSchema = new mongoose.Schema({
 	title: { type: String, required: true },
 	description: { type: String, required: true },
-	author: { type: String, default: "jm1", required: true },
+	author: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Users",
+		required: true,
+	},
 	created: { type: Date, default: Date.now, required: true },
 });
 module.exports = mongoose.model("Wiki", wikiSchema);
